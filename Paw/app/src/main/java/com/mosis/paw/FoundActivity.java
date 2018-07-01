41b3a58e27b8917d27e58d8c61fbf0cc70f81ba9
Unknown <nikolaaasavic@live.com>
2018-07-01 06:30:33 +0200

    Found activity

---
+++ Paw/app/src/main/java/com/mosis/paw/FoundActivity.java
@@ -0,0 +1,68 @@
+package com.mosis.paw;
+
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ScrollView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class FoundActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_found);
+
+        InitMap();
+
+        EditToolbar();
+    }
+
+    private void InitMap() {
+        ScrollableMapFragment mapFragment = (ScrollableMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.found_mapFragment);
+        mapFragment.getMapAsync(this);
+
+        final ScrollView scrollView = (ScrollView) findViewById(R.id.found_scroll_view);
+        ((ScrollableMapFragment) getSupportFragmentManager().findFragmentById(R.id.found_mapFragment)).setListener(new ScrollableMapFragment.OnTouchListener() {
+            @Override
+            public void onTouch() {
+                scrollView.requestDisallowInterceptTouchEvent(true);
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private void EditToolbar() {
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.colorAccent)));
+        getSupportActionBar().setTitle("Found");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
